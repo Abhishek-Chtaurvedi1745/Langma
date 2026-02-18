00599b7ee2aed878d942a2766e6ab197a5a5f32a
Abhishek-Chtaurvedi1745 <129581950+Abhishek-Chtaurvedi1745@users.noreply.github.com>
2026-02-18 12:57:37 +0530

    Change BrowserRouter basename to '/langma'
    jerhjdshj

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-<BrowserRouter>
+<BrowserRouter basename="/langma">
       <App />
     </BrowserRouter>
   </React.StrictMode>
